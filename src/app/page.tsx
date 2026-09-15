ae17ac015733fd6de2c682e502ebc793bf99af8e
lmveprog <matheuspro.contact@gmail.com>
2026-09-15 19:02:45 +0200

    ai content creation : 45k+ community

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -109,7 +109,7 @@ const projects: Project[] = [
   {
     title: "AI content creation",
     logo: "/images/projects/matheus.png",
-    desc: "Breaking down AI concepts and news for a wide audience.",
+    desc: "Breaking down AI concepts and news for a 45k+ community.",
     platforms: true,
   },
   {
